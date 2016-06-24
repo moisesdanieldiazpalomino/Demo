9ce463b548dc48f187a43b57860f0e4610301e39
moisesdanieldiazpalomino <moises.diaz.p@gmail.com>
2016-06-23 19:18:06 -0500

    Se agrega etiqueta se programa el boton

--- AppDemo 2/AppDemo 2/Form1.cs
+++ AppDemo 2/AppDemo 2/Form1.cs
@@ -15,5 +15,10 @@ namespace AppDemo_2
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("hola");
+        }
     }
 }
